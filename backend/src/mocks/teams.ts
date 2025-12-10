6498b661bf7eb352dab5342f9fb686ca99d1b900
Pietro Saccomando <pietro.saccomando@intesys.it>
2025-12-10 09:33:31 +0100

    feat: add mock data for drivers, teams, and tracks

---
+++ backend/src/mocks/teams.ts
@@ -0,0 +1,67 @@
+export const teams = [
+  {
+    teamId: "mercedes",
+    name: "Mercedes-AMG Petronas",
+    activeUntil: null
+  },
+  {
+    teamId: "redbull",
+    name: "Red Bull Racing",
+    activeUntil: null
+  },
+  {
+    teamId: "racingpoint",
+    name: "Racing Point",
+    activeUntil: 2020
+  },
+  {
+    teamId: "astonmartin",
+    name: "Aston Martin",
+    activeUntil: null
+  },
+  {
+    teamId: "ferrari",
+    name: "Scuderia Ferrari",
+    activeUntil: null
+  },
+  {
+    teamId: "renault",
+    name: "Renault F1 Team",
+    activeUntil: 2020
+  },
+  {
+    teamId: "alpine",
+    name: "BWT Alpine F1 Team",
+    activeUntil: null
+  },
+  {
+    teamId: "alphatauri",
+    name: "Scuderia AlphaTauri",
+    activeUntil: 2023
+  },
+  {
+    teamId: "racingbulls",
+    name: "Visa Cash App RB",
+    activeUntil: null
+  },
+  {
+    teamId: "alfaromeo",
+    name: "Alfa Romeo",
+    activeUntil: 2023
+  },
+  {
+    teamId: "kick",
+    name: "Stake F1 Team Kick Sauber",
+    activeUntil: null
+  },
+  {
+    teamId: "williams",
+    name: "Williams Racing",
+    activeUntil: null
+  },
+  {
+    teamId: "haas",
+    name: "Haas F1 Team",
+    activeUntil: null
+  }
+]
---
+++ backend/src/mocks/tracks.ts
@@ -0,0 +1,233 @@
+export const tracks = [
+  {
+    name: "Austria",
+    location: "Red Bull Ring",
+    country: "Austria",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Stiria",
+    location: "Red Bull Ring",
+    country: "Austria",
+    history: [2020, 2021],
+    is_active: false
+  },
+  {
+    name: "Ungheria",
+    location: "Hungaroring",
+    country: "Hungary",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Gran Bretagna",
+    location: "Silverstone Circuit",
+    country: "UK",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "70th Anniversario",
+    location: "Silverstone Circuit",
+    country: "UK",
+    history: [2020],
+    is_active: false
+  },
+  {
+    name: "Spagna",
+    location: "Circuit de Barcelona-Catalunya",
+    country: "Spain",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Belgio",
+    location: "Circuit de Spa-Francorchamps",
+    country: "Belgium",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Italia (Monza)",
+    location: "Autodromo Nazionale Monza",
+    country: "Italy",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Toscana",
+    location: "Autodromo Internazionale del Mugello",
+    country: "Italy",
+    history: [2020],
+    is_active: false
+  },
+  {
+    name: "Russia",
+    location: "Sochi Autodrom",
+    country: "Russia",
+    history: [2020, 2021],
+    is_active: false
+  },
+  {
+    name: "Nurburgring",
+    location: "Nürburgring",
+    country: "Germany",
+    history: [2020],
+    is_active: false
+  },
+  {
+    name: "Portogallo",
+    location: "Autódromo Internacional do Algarve",
+    country: "Portugal",
+    history: [2020, 2021],
+    is_active: false
+  },
+  {
+    name: "Imola",
+    location: "Autodromo Enzo e Dino Ferrari",
+    country: "Italy",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Turchia",
+    location: "Intercity Istanbul Park",
+    country: "Turkey",
+    history: [2020, 2021],
+    is_active: false
+  },
+  {
+    name: "Bahrein",
+    location: "Bahrain International Circuit (BIC)",
+    country: "Bahrain",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Sakhir",
+    location: "Bahrain International Circuit (Outer)",
+    country: "Bahrain",
+    history: [2020],
+    is_active: false
+  },
+  {
+    name: "Abu Dhabi",
+    location: "Yas Marina Circuit",
+    country: "UAE",
+    history: [2020, 2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Monaco",
+    location: "Circuit de Monaco",
+    country: "Monaco",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Azerbaijan",
+    location: "Baku City Circuit",
+    country: "Azerbaijan",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Francia",
+    location: "Circuit Paul Ricard",
+    country: "France",
+    history: [2021, 2022],
+    is_active: false
+  },
+  {
+    name: "Olanda",
+    location: "Circuit Zandvoort",
+    country: "Netherlands",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "USA (Austin)",
+    location: "Circuit of the Americas (COTA)",
+    country: "USA",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Messico",
+    location: "Autódromo Hermanos Rodríguez",
+    country: "Mexico",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Brasile",
+    location: "Autódromo José Carlos Pace",
+    country: "Brazil",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Qatar",
+    location: "Losail International Circuit",
+    country: "Qatar",
+    history: [2021, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Saudi Arabia",
+    location: "Jeddah Corniche Circuit",
+    country: "Saudi Arabia",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Australia",
+    location: "Albert Park Circuit",
+    country: "Australia",
+    history: [2021, 2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Miami",
+    location: "Miami International Autodrome",
+    country: "USA",
+    history: [2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Canada",
+    location: "Circuit Gilles Villeneuve",
+    country: "Canada",
+    history: [2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Singapore",
+    location: "Marina Bay Street Circuit",
+    country: "Singapore",
+    history: [2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Giappone",
+    location: "Suzuka International Racing Course",
+    country: "Japan",
+    history: [2022, 2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Las Vegas",
+    location: "Las Vegas Strip Circuit",
+    country: "USA",
+    history: [2023, 2024, 2025],
+    is_active: true
+  },
+  {
+    name: "Cina",
+    location: "Shanghai International Circuit",
+    country: "China",
+    history: [2024, 2025],
+    is_active: true
+  }
+]
